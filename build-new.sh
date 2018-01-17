1f13642f76d68da964852c038306aa52c277058e
Alen Kahvic <alen.kahvic@greenpeace.org>
2018-01-17 17:23:17 +0100

    build-new.sh: Removed one \n  from nginx sed expression.

--- build-new.sh
+++ build-new.sh
@@ -4,4 +4,4 @@ INFILE=redirects-new
 NGINX_OUT="nginx/redirects.conf"
 APACHE_OUT="apache/redirects.conf"
 
-sed -n -e 'h ; s_^/_Redirect permanent /_w '"$APACHE_OUT" -e 'g ; s_\(/international/en/\)\(\Whttp\)_ ~ ^\1.*$\2_ ; s/\(.*\)\W\(http.*\)$/location \1 {\n\t\treturn 301 \2\n}\n\n/w '"$NGINX_OUT" <"$INFILE"
+sed -n -e 'h ; s_^/_Redirect permanent /_w '"$APACHE_OUT" -e 'g ; s_\(/international/en/\)\(\Whttp\)_ ~ ^\1.*$\2_ ; s/\(.*\)\W\(http.*\)$/location \1 {\n\t\treturn 301 \2\n}\n/w '"$NGINX_OUT" <"$INFILE"
